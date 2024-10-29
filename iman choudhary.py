108a647a6461ae989960ccf9cc2b0bf39d39fdac
imanchoudhary101 <imanchoudhary814@gmail.com>
2024-10-29 20:59:54 +0300

    Add files via upload

---
+++ iman choudhary.py
@@ -0,0 +1,20 @@
+num1=74
+num2=87
+num3=234
+sum=num1+num2+num3
+print(sum)
+diff=num1-num2-num3
+print(diff)
+product=num1*num2*num3
+print(product)
+average=(num1+num2+num3)/3
+print(average)
+# area of triangle
+area=34*99/2
+print(area)
+# area of sqaure
+area=33*33
+print(area)
+# area of circle
+area=3.14*21*21
+print(area)
